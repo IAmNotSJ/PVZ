ba17135a9a2624e572580b0c4636c905db087717
IAmSJ <84604061+IAmNotSJ@users.noreply.github.com>
2025-04-20 14:44:33 -0400

    ATLAS TEXTURE CONVERSION
    almost

--- debug/animation_tester/misc/texture_atlas_converter.gd
+++ debug/animation_tester/misc/texture_atlas_converter.gd
@@ -28,25 +28,33 @@ const NAMES = {
 	"loop": "LP",
 	"Matrix3D": "M3D",
 	"metadata": "MD",
-	"framerate": "FRT"
+	"framerate": "FRT",
+	"SYMBOL_Instance" : "SI",
 }
 
 
 
 func _ready():
-	var anim_json = load("res://debug/animation_tester/misc/texture_atlas_converter/test/happy/Animation.json")
-	var spritemap_json = load("res://debug/animation_tester/misc/texture_atlas_converter/test/happy/spritemap1.json")
+	var anim_json = load("res://debug/animation_tester/misc/texture_atlas_converter/test/extradance/Animation.json")
+	var spritemap_json = load("res://debug/animation_tester/misc/texture_atlas_converter/test/extradance/spritemap1.json")
 	#print(anim_json.data)
 	#print(spritemap_json.data)
 	_load_atlas(anim_json, spritemap_json)
 
 func _load_atlas(anim_json:JSON, spritemap_json:JSON):
 	
+	var is_optimized:bool = false
+	if anim_json.data.has(NAMES["ANIMATION"]):
+		is_optimized = true
+	
+	var info:Dictionary = {
+		"Framrate" : anim_json.data[NAMES["metadata"]][NAMES["framerate"]]
+	}
+	
 	var symbols:Dictionary[String, Array] = {}
 	var rects:Dictionary[String, Rect2i] = {}
 	var frames:Dictionary[String, Dictionary] = {}
 	
-	var is_optimized:bool = false
 	
 	var sprites = []
 	for sprite in spritemap_json.data["ATLAS"]["SPRITES"]:
@@ -58,18 +66,43 @@ func _load_atlas(anim_json:JSON, spritemap_json:JSON):
 			int(sprite_info["h"])
 			)
 	
-	if anim_json.data.has(NAMES["SYMBOL_DICTIONARY"]):
-		for symbol_data in anim_json.data[NAMES["SYMBOL_DICTIONARY"]][NAMES["Symbols"]]:
-			symbols[symbol_data[NAMES["SYMBOL_name"]]] = symbol_data[NAMES["TIMELINE"]][NAMES["LAYERS"]]
-			symbols[symbol_data[NAMES["SYMBOL_name"]]].reverse()
+	var limbs
 	
+	for symbol_data in anim_json.data[NAMES["SYMBOL_DICTIONARY"]][NAMES["Symbols"]]:
+		symbols[symbol_data[NAMES["SYMBOL_name"]]] = symbol_data[NAMES["TIMELINE"]][NAMES["LAYERS"]]
 	symbols["_top"] = anim_json.data[NAMES["ANIMATION"]][NAMES["TIMELINE"]][NAMES["LAYERS"]]
-	symbols["_top"].reverse()
 	
+	
+	var recursive_symbols:Array[String] = []
 	for symbol in symbols.keys():
-		for layer in symbols[symbol]:
-			for frame in layer[NAMES["Frames"]]:
-				frames[symbol][NAMES["Frames"]] 
+		frames[symbol] = {}
+		var recursive_symbol:bool = true
+		if symbol != "_top":
+			var layers_array = symbols[symbol]
+			for layer in layers_array:
+				for frame in layer[NAMES["Frames"]]:
+					frames[symbol] 
+					for element in frame[NAMES["elements"]]:
+						if !element.has(NAMES["SYMBOL_Instance"]):
+							recursive_symbol = false
+		if !recursive_symbol:
+			var new_symbol = Sprite2D.new()
+			new_symbol.name = symbol
+			frames[symbol]["Symbol"] = new_symbol
+	print(frames)
+	#	print(frames)
+		#for layer in symbols[symbol]:
+			#for frame in layer[NAMES["Frames"]]:
+				#for element in frame[NAMES["elements"]]:
+					#match element.keys()[0]:
+						#NAMES["ATLAS_SPRITE_instance"]:
+							#frames[element[NAMES["ATLAS_SPRITE_instance"]][NAMES["name"]]] = {
+								#"Transform2D" : m3d_to_transform2d(element[NAMES["ATLAS_SPRITE_instance"]][NAMES["Matrix3D"]], is_optimized)
+							#}
+						#NAMES["SYMBOL_Instance"]:
+							#frames[element[NAMES["SYMBOL_Instance"]][NAMES["SYMBOL_name"]]] = {
+								#"Transform2D" : m3d_to_transform2d(element[NAMES["SYMBOL_Instance"]][NAMES["Matrix3D"]], is_optimized)
+							#}
 
 func m3d_to_transform2d(matrix, is_optimized) -> Transform2D:
 	var x_axis:Vector2
